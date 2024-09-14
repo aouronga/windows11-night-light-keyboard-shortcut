23b0a669fce8ee6b75e7dc4499b95a3a80d8dab1
Aourongajab Abir <abir5001@gmail.com>
2024-09-14 12:46:33 +0600

    Update NightLightToggle.bat

--- NightLightToggle.bat
+++ NightLightToggle.bat
@@ -1,7 +1,12 @@
 @echo off
 start ms-settings:nightlight
-timeout /t 2 /nobreak >nul
+
+REM If waiting is necessary, simply uncomment the line below (remove 'REM').
+REM timeout /t 1 /nobreak >nul
+
 powershell -command "$wshell = New-Object -ComObject wscript.shell; $wshell.SendKeys('{ENTER}')"
 
-timeout /t 1 /nobreak >nul
+REM If waiting is necessary, simply uncomment the line below (remove 'REM').
+REM timeout /t 1 /nobreak >nul
+
 powershell -command "Get-Process | Where-Object { $_.MainWindowTitle -like 'Settings' } | ForEach-Object { $_.CloseMainWindow() }"
